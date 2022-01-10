c926dbb6f7324721906e6bc39ba1c5b670778e0f
Irina Kuzmina <irina.alex.kuzmina@gmail.com>
2022-01-10 15:31:43 +0300

    task1

--- servlets/src/main/java/ru/netology/model/Post.java
+++ servlets/src/main/java/ru/netology/model/Post.java
@@ -1,30 +1,50 @@
 package ru.netology.model;
 
 public class Post {
-  private long id;
-  private String content;
+    private long id;
+    private String content;
 
-  public Post() {
-  }
+    public Post() {
+    }
 
-  public Post(long id, String content) {
-    this.id = id;
-    this.content = content;
-  }
+    public Post(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
 
-  public long getId() {
-    return id;
-  }
+    public Post(long id) {
+        this.id = id;
+        this.content = "";
+    }
 
-  public void setId(long id) {
-    this.id = id;
-  }
+    public long getId() {
+        return id;
+    }
 
-  public String getContent() {
-    return content;
-  }
+    public void setId(long id) {
+        this.id = id;
+    }
 
-  public void setContent(String content) {
-    this.content = content;
-  }
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Post post = (Post) o;
+
+        return id == post.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
 }
--- servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -2,24 +2,60 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
-  public List<Post> all() {
-    return Collections.emptyList();
-  }
 
-  public Optional<Post> getById(long id) {
-    return Optional.empty();
-  }
+    private Queue<Post> rep;
+    private AtomicLong counter = new AtomicLong(0L);
 
-  public Post save(Post post) {
-    return post;
-  }
+    public PostRepository() {
+        rep = new ConcurrentLinkedQueue<>();
+    }
 
-  public void removeById(long id) {
-  }
+    public List<Post> all() {
+        return new ArrayList<>(rep);
+    }
+
+    // если post с таким id не найден, то возвращаем пустой элемент
+    public Optional<Post> getById(long id) {
+        if (!rep.contains(new Post(id))) {
+            return Optional.empty();
+        }
+        return rep.stream().filter(x -> x.getId() == id).findFirst();
+    }
+
+    private void addPost(Post post) {
+        post.setId(counter.incrementAndGet());
+        rep.add(post);
+    }
+
+    // если post с таким id !=0 не найден, то добавляем этот post в коллекцию c id, равному следующему
+    // по порядку в коллекции, а иначе у нас будут пересечения, что не очень
+    public Post save(Post post) {
+        if (post.getId() != 0) {
+            if (rep.contains(post)) {
+                rep.remove(post);
+                rep.add(post);
+            } else {
+                addPost(post);
+            }
+        } else {
+            addPost(post);
+        }
+        return post;
+    }
+
+    // если post с таким id не найден, то возвращаем пустой элемент, иначе возвращаем удаляемый post
+    public Optional<Post> removeById(long id) {
+        if (!rep.contains(new Post(id))) {
+            return Optional.empty();
+        }
+        final var post = rep.stream().filter(x -> x.getId() == id).findFirst();
+        rep.remove(post.get());
+        return post;
+    }
 }
